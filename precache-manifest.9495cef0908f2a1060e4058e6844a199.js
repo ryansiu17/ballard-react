8dd22d84e028d9e52f7e144e5d41cee8b12f4a87
Ryan Alexander Siu <31353270+ryansiu17@users.noreply.github.com>
2018-12-02 16:49:21 -0800

    Updates

---
+++ precache-manifest.9495cef0908f2a1060e4058e6844a199.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c4fddadeb14b3cce17f76d7bac7e6de",
+    "url": "/ballard-react/static/media/ballardLumber.7c4fddad.jpg"
+  },
+  {
+    "revision": "5c78ee8c5af9c1203c5b",
+    "url": "/ballard-react/static/css/main.579bc4e0.chunk.css"
+  },
+  {
+    "revision": "1436c32406a0700bd640",
+    "url": "/ballard-react/static/js/1.1436c324.chunk.js"
+  },
+  {
+    "revision": "40f48d6ada0b20f26972",
+    "url": "/ballard-react/static/js/runtime~main.40f48d6a.js"
+  },
+  {
+    "revision": "379df062887c8c7b09c7dbb3b2c7f326",
+    "url": "/ballard-react/static/media/ballardLocks.379df062.jpg"
+  },
+  {
+    "revision": "172f210d84da7f2d5320a95661c303f4",
+    "url": "/ballard-react/static/media/ballardStreet.172f210d.jpg"
+  },
+  {
+    "revision": "5c78ee8c5af9c1203c5b",
+    "url": "/ballard-react/static/js/main.5c78ee8c.chunk.js"
+  },
+  {
+    "revision": "fc354686d49b4a475326fcd1cfacdb0c",
+    "url": "/ballard-react/static/media/ballardBuildings.fc354686.jpg"
+  },
+  {
+    "revision": "f5e02455f779e275a218806f627d6098",
+    "url": "/ballard-react/static/media/ballardBell.f5e02455.jpg"
+  },
+  {
+    "revision": "d3e02c3268761be5073fbbaf9fe294cd",
+    "url": "/ballard-react/static/media/ballardOld.d3e02c32.jpg"
+  },
+  {
+    "revision": "01c0565061a25f953df4c6cab81f7e82",
+    "url": "/ballard-react/static/media/us.01c05650.jpg"
+  },
+  {
+    "revision": "ec993817257d67219f970f66e49018f4",
+    "url": "/ballard-react/static/media/ballardNordicMuseum.ec993817.jpg"
+  },
+  {
+    "revision": "f558e4897aebf5d07c298b3b0ea1301a",
+    "url": "/ballard-react/index.html"
+  }
+];
